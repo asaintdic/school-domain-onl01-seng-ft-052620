62eff2edf16ef2d915225410bd4d25f55b8d38c4
Alain Saint-Dic <alains214@gmail.com>
2020-06-06 04:56:57 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -6,23 +6,7 @@ class School
   
   def intialize(name)
      @name = name 
-  end
-  def name 
-     @name
-  end 
-  def roster=(roster)
-      roster = Hash.new
-    @roster = roster
-  end
-  def roster
-    @roster
-  end
-  def add_student=(add_student)
-    @add_student = add_student
-  end 
-  def add_student(student, grade)
-   @add_student
-  end
+  
 end
 
 # code here!
